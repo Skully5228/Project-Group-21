d2682e300cb6e13d88072ae0162a7517aee87dd9
itziac <itziaacortes@gmail.com>
2025-04-07 23:09:49 -0400

    Update database.js

--- back-end/database.js
+++ back-end/database.js
@@ -1,4 +1,3 @@
-// db.js or similar file in your backend folder
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
@@ -11,5 +10,25 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0,
 });
+
+// Create the database if it doesn't exist
+async function createDatabaseIfNeeded() {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+  });
+
+  await connection.query(`
+    CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME};
+  `);
+
+  connection.end();
+}
+
+// Run the function to ensure the database exists
+createDatabaseIfNeeded()
+  .then(() => console.log('Database is ready'))
+  .catch((err) => console.error('Error creating database:', err));
 
 module.exports = pool;
